f6f02b1d53b1f75d09cd7f99294ea56cf33b3bd8
Mikołaj Kuranowski <mkuranowski@gmail.com>
2026-02-22 11:20:27 +0100

    Fix trip merging

--- korail_gtfs/load_schedules.py
+++ korail_gtfs/load_schedules.py
@@ -2,17 +2,18 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import re
-from collections.abc import Iterable, Mapping
-from dataclasses import dataclass, field
-from operator import itemgetter
-from typing import Self, cast
+from collections import defaultdict
+from collections.abc import Iterable, Sequence
+from dataclasses import dataclass, field, replace
+from operator import attrgetter
+from typing import cast
 
 from impuls import DBConnection, Task, TaskRuntime
 from impuls.errors import DataError, MultipleDataErrors
 from impuls.model import Calendar, Date, StopTime, TimePoint, Trip
 from impuls.tools.types import StrPath
 
-from .scrape import ScrapedTrip, scrape_from_xlsx
+from .scrape import ScrapedStopTime, ScrapedTrip, scrape_from_xlsx
 
 KOREAN_WEEKDAYS_TO_MASK = {c: 1 << i for i, c in enumerate("월화수목금토일")}
 
@@ -20,7 +21,7 @@ SECOND = 1
 MINUTE = 60 * SECOND
 HOUR = 60 * MINUTE
 DAY = 24 * HOUR
-LAST_NIGHT_CUTOFF = 22 * HOUR
+LATE_NIGHT_CUTOFF = 22 * HOUR
 EARLY_MORNING_CUTOFF = 4 * HOUR
 
 
@@ -35,36 +36,6 @@ class _ConvertedTrips:
             db.create_many(Calendar, self.calendars)
             db.create_many(Trip, self.trips)
             db.create_many(StopTime, self.stop_times)
-
-
-@dataclass
-class _CombinedScrapedTrip:
-    # TODO: Don't assume a train stops only once at a specific station.
-    #       Some trains on the Seohae line stop twice at Hongseong (like 1241)
-
-    number: str
-    kind: str
-    note: str
-    stops: dict[str, int] = field(default_factory=dict[str, int])
-
-    @classmethod
-    def from_scraped_trip(cls, t: ScrapedTrip) -> Self:
-        return cls(
-            number=t.number,
-            kind=t.kind,
-            note=t.note,
-            stops={_slugify(i.stop): i.time for i in t.stops},
-        )
-
-    def merge_with(self, t: ScrapedTrip) -> None:
-        assert self.number == t.number
-        if self.kind != t.kind:
-            raise DataError(f"Trip {self.number} has different kinds: {self.kind} and {t.kind}")
-        if self.note != t.note:
-            raise DataError(f"Trip {self.number} has different notes: {self.note} and {t.note}")
-
-        for i in t.stops:
-            self.stops.setdefault(_slugify(i.stop), i.time)
 
 
 class LoadSchedules(Task):
@@ -104,7 +75,7 @@ class LoadSchedules(Task):
 
     def convert_scraped_trips(self, scraped: Iterable[ScrapedTrip]) -> _ConvertedTrips:
         converted = _ConvertedTrips()
-        for trip in _deduplicate_scraped_trips(scraped).values():
+        for trip in _deduplicate_scraped_trips(scraped):
             # Create the calendar
             # TODO: Create more readable calendar_id
             calendar_id = _note_to_calendar_mask(trip.note)
@@ -155,26 +126,30 @@ class LoadSchedules(Task):
     def match_route(self, kind: str) -> str:
         return self.routes[_slugify(kind)]
 
-    def match_stops(self, trip_id: str, stops: Mapping[str, int]) -> Iterable[StopTime]:
+    def match_stops(self, trip_id: str, scraped: list[ScrapedStopTime]) -> Iterable[StopTime]:
         # Check if trip runs late at night, and if so, shift past-midnight departures
-        if any(i >= LAST_NIGHT_CUTOFF for i in stops.values()):
-            stops = {
-                name: (time + DAY if time < EARLY_MORNING_CUTOFF else time)
-                for name, time in stops.items()
-            }
+        if any(i.time >= LATE_NIGHT_CUTOFF for i in scraped):
+            for st in scraped:
+                if st.time < EARLY_MORNING_CUTOFF:
+                    st.time += DAY
 
-        for idx, (name, time) in enumerate(sorted(stops.items(), key=itemgetter(1))):
-            if stop_id := self.stops.get(name):
-                timepoint = TimePoint(seconds=time)
+        # Ensure stop-times are sorted on time
+        scraped.sort(key=attrgetter("time"))
+
+        # Generate impuls StopTime objects
+        for i, st in enumerate(scraped):
+            stop_slug = _slugify(st.stop)
+            if stop_id := self.stops.get(stop_slug):
+                timepoint = TimePoint(seconds=st.time)
                 yield StopTime(
                     trip_id=trip_id,
-                    stop_sequence=idx,
+                    stop_sequence=i,
                     stop_id=stop_id,
                     arrival_time=timepoint,
                     departure_time=timepoint,
                 )
             else:
-                self.unknown_stops.add(name)
+                self.unknown_stops.add(stop_slug)
 
     def check_for_unknown_stops(self) -> None:
         if self.unknown_stops:
@@ -194,14 +169,42 @@ def _create_route_lookup(db: DBConnection) -> dict[str, str]:
         return {_slugify(cast(str, i[1])): cast(str, i[0]) for i in query}
 
 
-def _deduplicate_scraped_trips(scraped: Iterable[ScrapedTrip]) -> dict[str, _CombinedScrapedTrip]:
-    unique = dict[str, _CombinedScrapedTrip]()
-    for t in scraped:
-        if existing := unique.get(t.number):
-            existing.merge_with(t)
+def _deduplicate_scraped_trips(scraped: Iterable[ScrapedTrip]) -> Iterable[ScrapedTrip]:
+    # Group trips by the number
+    by_number = defaultdict[str, list[ScrapedTrip]](list)
+    for trip in scraped:
+        by_number[trip.number].append(trip)
+
+    # Deduplicate them
+    for group in by_number.values():
+        yield _merge_scraped_trips(group)
+
+
+def _merge_scraped_trips(trips: Sequence[ScrapedTrip]) -> ScrapedTrip:
+    # Fast path when there's nothing to merge
+    if len(trips) == 1:
+        return trips[0]
+
+    # Check that all trains have the same details
+    numbers = set(i.number for i in trips)
+    kinds = set(i.kind for i in trips)
+    notes = set(i.note for i in trips)
+    if len(numbers) != 1 or len(kinds) != 1 or len(notes) != 1:
+        raise ValueError(f"can't merge trains with different details: {numbers=} {kinds=} {notes=}")
+
+    # Merge all stop-times together
+    all_stop_times = [j for i in trips for j in i.stops]
+    all_stop_times.sort(key=attrgetter("time"))
+
+    # Merge consecutive stop-times at the same stop
+    stop_times = list[ScrapedStopTime]()
+    for st in all_stop_times:
+        if stop_times and stop_times[-1].stop == st.stop:
+            stop_times[-1].time = st.time
         else:
-            unique[t.number] = _CombinedScrapedTrip.from_scraped_trip(t)
-    return unique
+            stop_times.append(st)
+
+    return replace(trips[0], stops=stop_times)
 
 
 def _note_to_calendar_mask(note: str) -> int:
